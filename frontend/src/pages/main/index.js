284a58b481663a2e3d67d1f2ba88c978fee86e7e
victor <victorabelo1697@gmail.com>
2020-05-31 22:34:46 -0300

    Armazenando no estado

---
+++ frontend/src/pages/main/index.js
@@ -0,0 +1,63 @@
+import React, { useState, useEffect } from 'react';
+import api from '../../services/api';
+import { Link } from 'react-router-dom';
+
+import './styles.css';
+
+export default function Main() {
+  const [products, setProducts] = useState([]);
+  const [producInfo, setProductInfo] = useState({});
+  const [page, setPage] = useState(1);
+
+
+  useEffect(() => {
+    loadProducts();
+  }, []);
+
+
+  loadProducts = async (page = 1) => {
+    const response = await api.get(`/products?page=${page}`);
+
+    const { docs, ...productInfo } = response.data;
+
+    setProducts(docs);
+    setProductInfo(producInfo);
+    setPage(page);
+  }
+
+  prevPage = () => {
+    if (page === 1) return;
+
+    const pageNumber = page - 1;
+
+    loadProducts(pageNumber);
+  }
+
+  nextPage = () => {
+    if (page === productInfo.pages) return;
+
+    const pageNumber = page + 1;
+
+    loadProducts(pageNumber);
+  }
+
+  return (
+    <div className="container">
+      <Link id="router" to={'/create'}>Criar</Link>
+      <div className="product-list">
+        {products.map(product => (
+          <article key={product._id} >
+            <strong>{product.title}</strong>
+            <p>{product.description}</p>
+
+            <Link to={`/products/${product._id}`} >Acessar</Link>
+          </article>
+        ))}
+        <div className="actions">
+          <button disabled={page === 1} onClick={prevPage}>Anterior</button>
+          <button disabled={page === productInfo.pages} onClick={nextPage}>Próxima</button>
+        </div>
+      </div>
+    </div>
+  );
+}
