b43dc41440afd80d4fb43737f00d6f9618be2fc9
victor <victorabelo1697@gmail.com>
2020-05-31 21:59:50 -0300

    Criando Header

---
+++ frontend/src/components/Header/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import "./styles.css";
+
+const Header = () => (
+  <div id="main-div">
+    <a id="home" href="http://localhost:3000">
+      <img
+        height="30px"
+        src="https://cdn4.iconfinder.com/data/icons/contact-us-19/48/83-512.png"
+      />
+    </a>
+    <header id="main-header"> JSHunt</header>
+  </div>
+);
+
+export default Header;
